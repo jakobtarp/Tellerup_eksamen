2d0d87a867d674d05879567d004fb9a764774219
Jakob Tarp <jakob.tarp@outlook.com>
2023-05-22 10:48:33 +0200

    forsøg

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,23 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    colors: {
-      white: "#ffffff",
-      black: "#000",
-
-      vanilla: "#F6F4F0",
-      darkvanilla: "#DFD3C7",
-      green: "#00c154",
-      darkgreen: "#5B7F58",
-      lightblue: "#BBDBF3",
-      lighterblue: "#C4DDF1",
-      middleblue: "#5D7791",
-      darkerblue: "#071522",
-      royalblue: "#113759",
-      lightgrey: "#898989",
-      bluegrey: "#465A6F",
-      darkgrey: "#667085",
-      orange: "#BD492F",
+    extend: {
+      backgroundImage: {
+        "bg-discover": "url('imgs/tintindiscover.jpg')",
+      },
     },
   },
   plugins: [],
